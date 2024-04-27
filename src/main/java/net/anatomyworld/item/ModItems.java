70203c198cd747ddaba8f70ae8554c4f4f624b32
anatomyking <ps219772@summacollege.nl>
2024-04-27 21:12:03 +0200

    -added king seeds

--- src/main/java/net/anatomyworld/item/ModItems.java
+++ src/main/java/net/anatomyworld/item/ModItems.java
@@ -1,13 +1,21 @@
 package net.anatomyworld.item;
 
 import net.anatomyworld.woodland.WoodLand;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item KINGSEEDS = registerItem("king_seeds", new Item(new FabricItemSettings()));
 
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(KINGSEEDS);
+    }
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(WoodLand.MOD_ID, name), item);
@@ -16,5 +24,7 @@ public class ModItems {
 
     public static  void registerModItems() {
         WoodLand.LOGGER.info("Register Mod Items for" + WoodLand.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModItems::addItemsToIngredientTabItemGroup);
     }
 }
